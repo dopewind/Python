a6b23cc08e665128771c2edcb640aa0c68720bfb
dopewind <imkvinayan@gmail.com>
2020-10-12 15:53:59 +0530

    initial

---
+++ Beta.py
@@ -0,0 +1,5 @@
+import Alpha
+from Alpha import alpha, typing_effect_ideal, typing_effect_ideal_line, typing_effect_really_fast, typing_effect_really_fast_line, typing_effect_really_slow, typing_effect_really_slow_line
+
+
+alpha()
